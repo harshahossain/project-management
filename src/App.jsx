0cffdda97ba9be2115363b732b48433f4b4f1c7e
harshahossain <harshahossain@gmail.com>
2023-11-13 23:43:18 +0600

    updated ProjectSidebar.jsx
    updated Sidebar to show Projects

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ function App() {
     setProjectState((prevState) => {
       return {
         ...prevState,
+        selectedProjectID: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -51,7 +52,10 @@ function App() {
   //
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
